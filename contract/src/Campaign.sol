0982b86c5f4fb7ea9bf31ee0d3a9cc1a5cf48dd6
Petar Komatina <petarkkomatina00@gmail.com>
2026-07-15 10:57:56 +0200

    Added donator count. Fixed some require statements. Added finalize request operation

--- contract/src/Campaign.sol
+++ contract/src/Campaign.sol
@@ -11,6 +11,7 @@ contract Campaign{
     uint public minimumContribution;
     mapping(address => uint256) public donatorTotalContribution;
     mapping(address => bool) public donators;
+    uint256 public donatorsCount;
 
     modifier restricted(){
         require(msg.sender == manager, "Only manager can perform this action");
@@ -24,10 +25,16 @@ contract Campaign{
     function contribute() public payable{
         require(msg.value >= minimumContribution, "Donation is below minimum");
         
+        if(!donators[msg.sender]){
+            donators[msg.sender] = true;
+            donatorsCount++;
+        }
         donatorTotalContribution[msg.sender] += msg.value;
-        donators[msg.sender] = true;
     }
     function createRequest(string memory description, uint value, address recipient) public restricted(){
+        require(bytes(description).length > 0, "Description is required");
+        require(value > 0, "Value must be greater than zero");
+        require(recipient != address(0), "Invalid recipient");
         requests.push();
 
         CampaignRequest storage newRequest = requests[requests.length - 1];
@@ -38,11 +45,29 @@ contract Campaign{
         newRequest.approvalCount = 0;
     }
     function approveRequest(uint index) public{
-        require(donators[msg.sender]);
-        require(!requests[index].approvedBy[msg.sender]);
+        require(donators[msg.sender], "Only donators can approve");
+        require(!requests[index].approvedBy[msg.sender], "Request already approved");
 
         requests[index].approvedBy[msg.sender] = true;
         requests[index].approvalCount++;
+    }
+    function finalizeRequest(uint index) public{
+        require(index < requests.length, "Request does not exist");
+
+        CampaignRequest storage request = requests[index];
+        require(!request.complete, "Request is already complete");
+        require(request.approvalCount > donatorsCount / 2, "Not enought approvals");
+        require(address(this).balance >= request.value, "Insufficient campaign balance");
+
+        request.complete = true;
+
+        (bool sucesses, ) = request.recipient.call{
+            value: request.value
+        }("");
+
+        require(sucesses, "Transfer failed");
+
+        //Potreban event
     }
 
 
--- contract/src/factory/CampaignFactory.sol
+++ contract/src/factory/CampaignFactory.sol
@@ -7,6 +7,7 @@ contract CampaignFactory{
     address[] public deployedCampaigns;
 
     function createCampaign(uint minContribution) public returns(address){
+        require(minContribution > 0, "Minimum contribution must be greater than zero");
         Campaign newCampaign = new Campaign(minContribution, msg.sender);
         deployedCampaigns.push(address(newCampaign));
 
